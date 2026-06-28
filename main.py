3747d5255109fc8d9054a74b161c2f077b80f39c
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 19:56:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from handlers import admin_panel, commands, anti_spam
-
-TOKEN = "8787399797:AAFFPGgLOqo7hY9hsfzya9XbTf79Ra0DsXU"
+from handlers import admin_panel, anti_spam
 
 async def main():
-    bot = Bot(token=TOKEN)
+    bot = Bot(token="ضع_التوكن_هنا") # ضع التوكن هنا
     dp = Dispatcher()
-    
     dp.include_router(admin_panel.router)
-    dp.include_router(commands.router)
     dp.include_router(anti_spam.router)
     
-    logging.info("البوت يعمل الآن بنجاح")
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
