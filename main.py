076dcec7ced6de8407e7acde6e89251b06fecf4d
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 13:04:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from pyrogram import Client
-import os
 
-# تشغيل البوت مع ربط المجلد (handlers)
 app = Client(
     "ProtectionBot",
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH"),
-    bot_token=os.environ.get("BOT_TOKEN"),
-    plugins=dict(root="handlers") 
+    api_id=24752047,             # ضع رقمك الحقيقي هنا
+    api_hash="5b8a468627791bdd36a8c361913b0b72",   # ضع الـ Hash الحقيقي هنا
+    bot_token="8787399797:AAFFPGgLOqo7hY9hsfzya9XbTf79Ra0DsXU", # ضع التوكن الحقيقي هنا
+    plugins=dict(root="handlers")
 )
 
-if __name__ == "__main__":
-    print("ProtectionBot is starting...")
-    app.run()
+app.run()
