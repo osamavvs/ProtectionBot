cbfe28b3df8f020792eca2a891c56fa5b8703969
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 08:31:14 +0300

    Distinguish admin user in start command response

--- handlers/start.py
+++ handlers/start.py
@@ -2,14 +2,18 @@ from aiogram import Router, types
 from aiogram.filters import CommandStart
 
 from db import add_user
+from config import ADMIN_ID
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: types.Message):
+
     # تسجيل المستخدم
     add_user(message.from_user.id)
 
-    await message.answer(
-        "👋 أهلاً بيك!\nالبوت شغال بنجاح 🔥"
-    )
+    # تمييز الأدمن
+    if message.from_user.id == ADMIN_ID:
+        await message.answer("👑 أهلاً أدمن\nلوحة التحكم قادمة 🔥")
+    else:
+        await message.answer("👋 أهلاً بيك\nالبوت شغال بنجاح 🔥")
