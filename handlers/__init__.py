fb0abb889bc6bfbdc4b3ce74a2d9ac4745c3c0eb
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 11:48:01 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,10 @@
+from . import admin, callbacks, locks, start
+from aiogram import Dispatcher
 
+def setup_routers(dp: Dispatcher):
+    dp.include_routers(
+        admin.router,
+        callbacks.router,
+        locks.router,
+        start.router
+    )
