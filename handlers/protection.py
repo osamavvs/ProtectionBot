3223efc35efb9a695a4233e516a194be4402f1b1
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 16:31:19 +0300

    Delete handlers directory

--- handlers/protection.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.group & filters.regex(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"))
-async def delete_links(client, message):
-    # مسح الرابط تلقائياً
-    await message.delete()
-    await message.reply(f"🚫 تم حذف الرابط يا {message.from_user.mention}، يمنع نشر الروابط!")
