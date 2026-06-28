66424e8a0a20a0655a36c9b12fbd577f6a9a656d
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 11:30:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import admin, start, locks
+from database import init_db # استيراد دالة الإنشاء
 
 async def main():
+    await init_db() # تشغيل قاعدة البيانات عند بدء البوت
     bot = Bot(token="8787399797:AAFFPGgLOqo7hY9hsfzya9XbTf79Ra0DsXU")
     dp = Dispatcher()
 
-    # تسجيل الراوترات (إضافة الأجزاء)
     dp.include_routers(start.router, admin.router, locks.router)
-
-    print("البوت يعمل الآن يا أسامة...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
