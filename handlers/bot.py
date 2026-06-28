88d59413f0279f7b747f5f99e3a167752b2845ac
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 12:05:04 +0300

    Update bot.py

--- handlers/bot.py
+++ handlers/bot.py
@@ -5,8 +5,8 @@ import os
 app = Client(
     "PyroHub",
     api_id=123456, # ضع الأرقام الخاصة بك
-    api_hash="YOUR_HASH",
-    bot_token="YOUR_TOKEN",
+    api_hash="5b8a468627791bdd36a8c361913b0b72",
+    bot_token="8787399797:AAFFPGgLOqo7hY9hsfzya9XbTf79Ra0DsXU",
     plugins=dict(root="handlers") # هذا السطر هو سر سورس الموسوي (يقرأ كل الملفات تلقائياً)
 )
 
