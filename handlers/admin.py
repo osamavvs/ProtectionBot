0fcd60b152699d0d1213f0cf9bd2dd2ecf5471dd
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 11:35:20 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,17 +1,16 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-from database import set_links_lock # استيراد دالة التحديث
+from aiogram import Router, F, types
+from database import set_links_lock
 
 router = Router()
 
-@router.message(Command("قفل_الروابط"))
+# الأمر بدون سلاش
+@router.message(F.text == "قفل الروابط")
 async def lock_links_command(message: types.Message):
-    # تفعيل القفل (1 يعني مفعل)
     await set_links_lock(message.chat.id, 1)
-    await message.answer("تم تفعيل قفل الروابط في هذه المجموعة.")
+    await message.answer("تم تفعيل قفل الروابط.")
 
-@router.message(Command("فتح_الروابط"))
+# الأمر بدون سلاش
+@router.message(F.text == "فتح الروابط")
 async def unlock_links_command(message: types.Message):
-    # تعطيل القفل (0 يعني معطل)
     await set_links_lock(message.chat.id, 0)
     await message.answer("تم إلغاء تفعيل قفل الروابط.")
