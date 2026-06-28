2e3abff50d7f8a25dcef2f7e424983c2c50a2f50
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-06-28 17:13:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+import os
 
+from handlers import commands
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+
+    # تسجيل الهاندلرز
+    dp.include_router(commands.router)
+
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
